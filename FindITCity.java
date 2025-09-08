309236cc841914dfe59b6d3359c03f185f928da8
Lakshmi Sravani Indraganti <149859463+ILAKSHMISRAVANI@users.noreply.github.com>
2025-09-08 22:37:44 +0530

    FindITCity.java

--- FindITCity.java
+++ FindITCity.java
@@ -1,11 +1,4 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
-import java.util.*;
+//Find Entered City is IT city or not
 public class FindITCity
 {
 	public static void main(String[] args) {
